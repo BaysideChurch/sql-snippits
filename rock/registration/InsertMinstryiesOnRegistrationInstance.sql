fc98797ae2a6e2e0e4cbbbcae318928035e5bcd4
Eric Stoutenburg <eric@ericstoutenburg.com>
2023-04-19 08:59:00 -0400

    Add some sql to transfer ministries to reg instances

---
+++ rock/registration/InsertMinstryiesOnRegistrationInstance.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+    [AttributeValue] av
+VALUES
+(
+    SELECT
+      42056 AS [AttributeId],
+      ri.[Id] AS [EntityId],
+      existingAv.[Value],
+      GETDATE() AS [CreatedDateTime],
+      NEWID() AS [Guid],
+      dv.[Value] AS [PersistedValueText],
+      dv.[Value] AS [PersistedHtmlValue],
+      dv.[Value] AS [PersistedCondensedTextValue],
+      dv.[Value] AS [PersistedCondensedHtmlValue],
+      existingAv.[ValueChecksum]
+  FROM
+      [AttributeValue] existingAv
+      INNER JOIN [EventItemOccurrence] eic ON eic.[Id] = existingAv.[EntityId]
+      INNER JOIN [EventItemOccurrenceGroupMap] eicgm ON eic.[Id] = eicgm.[EventItemOccurrenceId]
+      INNER JOIN [RegistrationInstance] ri ON ri.[Id] = eicgm.[RegistrationInstanceId]
+      INNER JOIN [DefinedValue] dv ON existingAv.[Value] = dv.[Guid]
+  WHERE
+      existingAv.[AttributeId] = 17906
+)
