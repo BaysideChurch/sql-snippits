15b86e5f5d6153ecafb23ba1859ad6f26bc6b293
Eric Stoutenburg <eric@ericstoutenburg.com>
2023-04-26 20:59:05 -0400

    More changes

---
+++ rock/registration/EventSearchQueryWithVariables.sql
@@ -0,0 +1,104 @@
+--
+-- Just like the event search query but with the page parameter variables declared for easy testing
+--
+
+DECLARE @farInPast DATE = DATEADD(YEAR, -20, GETDATE());
+DECLARE @farinFuture DATE = DATEADD(YEAR, 20, GETDATE());
+
+SELECT
+  TOP 1000
+  ri.[Name] AS [InstanceName],
+  rt.[Name] AS [TemplateName],
+  ri.[Id] AS Id,
+  ri.[StartDateTime] AS RegistrationStart,
+  ri.[EndDateTime] AS RegistrationClose,
+  s.[EffectiveStartDate] AS EventStart,
+  COALESCE(s.[EffectiveEndDate], s.[EffectiveStartDate]) AS EventEnd,
+  c.[Name] AS [Campus]
+FROM
+  [RegistrationInstance] ri
+  INNER JOIN [RegistrationTemplate] rt ON ri.[RegistrationTemplateId] = rt.[Id]
+  LEFT JOIN [AttributeValue] riMinistryAv ON riMinistryAv.[EntityId] = ri.[Id] AND riMinistryAv.[AttributeId] = 42056
+  LEFT JOIN [AttributeValue] riCampusesAv ON riCampusesAv.[EntityId] = ri.[Id] AND riCampusesAv.[AttributeId] = 42057
+  LEFT JOIN [Campus] c ON c.[Guid] = TRY_CONVERT(UNIQUEIDENTIFIER, @campus)
+  LEFT JOIN [EventItemOccurrenceGroupMap] eicgm ON ri.[Id] = eicgm.[RegistrationInstanceId]
+  LEFT JOIN [EventItemOccurrence] eic ON eic.[Id] = eicgm.[EventItemOccurrenceId]
+  LEFT JOIN [Schedule] s ON eic.[ScheduleId] = s.[Id]
+  LEFT JOIN [AttributeValue] eicMinistryAv ON eicMinistryAv.[EntityId] = eic.[Id] AND eicMinistryAv.[AttributeId] = 17906
+WHERE
+  -- filter based on ministry
+  (
+    @ministry = '' OR
+    eicMinistryAv.[Value] = @ministry OR
+    riMinistryAv.[Value] = @ministry
+  ) AND
+
+  -- filter based on template name
+  (
+    @templateName = '' OR
+    rt.[Name] LIKE (
+      '%' + @templateName + '%' -- match in middle of word
+    ) OR
+    rt.[Name] LIKE (
+      @templateName + '%' -- match prefix
+    )
+  ) AND
+
+  -- filter based on instance name
+  (
+    @instanceName = '' OR
+    ri.[Name] LIKE (
+      '%' + @instanceName + '%' -- match in middle of word
+    ) OR
+    ri.[Name] LIKE (
+      @instanceName + '%' -- match prefix
+    )
+  ) AND
+
+  -- filter based on end date
+  (
+    @startDate = '' OR
+    DATEDIFF(
+      day, 
+      COALESCE(s.[EffectiveEndDate], s.[EffectiveStartDate]),
+      (
+        CASE
+        WHEN (@startDate = '') THEN @farInPast
+        WHEN (@startDate != '') THEN CONVERT(DATE, @startDate)
+        END
+      )
+    ) <= 0
+  ) AND
+
+  -- filter based on start date
+  (
+    @endDate = '' OR
+    DATEDIFF(
+      day, 
+      s.[EffectiveStartDate],
+      (
+        CASE
+        WHEN (@endDate = '') THEN @farInFuture
+        WHEN (@endDate != '') THEN CONVERT(DATE, @endDate)
+        END
+      )
+    ) >= 0
+  ) AND
+
+  (
+    -- filter based on campus (RegistrationInstance attribute)
+    (
+      @campus = '' OR
+      @campus IN (SELECT value FROM STRING_SPLIT(riCampusesAv.[Value], ','))
+    ) OR
+
+    -- filter based on campus (EventItemOccurrence CampusId)
+    (
+      @campus = '' OR
+      c.[Id] = eic.[CampusId]
+    )
+  )
+
+ORDER BY
+  s.[EffectiveStartDate] DESC,
+  ri.[EndDateTime] DESC
